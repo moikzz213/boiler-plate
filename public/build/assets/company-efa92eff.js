c960dd8fa7dea5069dc7bcb3698371bfa5e84eb9
moikzz213 <jacob@gagroup.net>
2023-07-20 09:18:57 +0400

    Fixed kpi dialog

---
+++ public/build/assets/company-efa92eff.js
@@ -0,0 +1,1 @@
+import{a3 as e}from"./app-9a1a3cff.js";import{c as i}from"./clientApi-79bcd4c6.js";const n=e("company",{state:()=>({company_list:[]}),getters:{companies:t=>t.company_list,active_companies:t=>t.company_list.filter(a=>a.status==="active")},actions:{async getCompanies(t){await i(t).get("/api/companies").then(a=>{this.company_list=a.data})}}});export{n as u};
