0c74f3f1fc0a377872830665f58c6c8d2ffef75e
moikzz213 <jacob@gagroup.net>
2023-07-25 15:15:47 +0400

    Add final rating title in Printing

--- public/build/assets/industry-044d3840.js
+++
@@ -1,1 +0,0 @@
-import{a3 as i}from"./app-e975ce6f.js";import{c as e}from"./clientApi-523cee1d.js";const a=i("industry",{state:()=>({industry_list:[]}),getters:{industries:t=>t.industry_list,active_industries:t=>t.industry_list.filter(s=>s.status==="active")},actions:{async getIndustries(t){await e(t).get("/api/industries").then(s=>{this.industry_list=s.data})}}});export{a as u};
