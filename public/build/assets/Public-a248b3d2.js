922ac66b28e871f02e7b0a1abe86b3b9ddff7aac
moikzz213 <jacob@gagroup.net>
2023-07-27 15:16:07 +0400

    text align left report

--- public/build/assets/Public-a248b3d2.js
+++
@@ -1,1 +0,0 @@
-import{ax as e,o as r,c}from"./app-8b546cee.js";import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";const n={};function s(_,a){const o=e("router-view");return r(),c(o)}const m=t(n,[["render",s]]);export{m as default};
