47edcebc995fefe3eacd0a55cee4a6c8fa7339b4
moikzz213 <jacob@gagroup.net>
2023-07-27 10:50:03 +0400

    Fixed alignment

---
+++ public/build/assets/company-11f205f7.js
@@ -0,0 +1,1 @@
+import{a3 as e}from"./app-7ac39acb.js";import{c as i}from"./clientApi-eccfa06b.js";const n=e("company",{state:()=>({company_list:[]}),getters:{companies:t=>t.company_list,active_companies:t=>t.company_list.filter(a=>a.status==="active")},actions:{async getCompanies(t){await i(t).get("/api/companies").then(a=>{this.company_list=a.data})}}});export{n as u};
